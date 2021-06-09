5e0caf48f2c48e3ca20489e5c7c8be5d931fe656
Ellen Langelaar <ellen.langelaar@nedap.com>
2021-06-09 11:32:00 +0200

    Testing intput

--- dist/index.js
+++ dist/index.js
@@ -6176,45 +6176,22 @@ async function getRelease(octokit) {
     ...github.context.repo,
   });
 
-  console.log(`${JSON.stringify(releases, undefined, 2)}`); // REMOVE THIS
+  console.log(`${JSON.stringify(releases.data[0], undefined, 2)}`); // REMOVE THIS
 
   return releases.data[0];
 }
 
 async function addAsset(release, octokit) {
   const assetPath = core.getInput("asset-path");
-  //const assetContentType = core.getInput("asset-content-type");
-  //const uploadUrl = release.upload_url;
-
-  // // Determine content-length for header to upload asset
-  // const contentLength = (filePath) => fs.statSync(filePath).size;
-
-  // // Setup headers for API call, see Octokit Documentation: https://octokit.github.io/rest.js/#octokit-routes-repos-upload-release-asset for more information
-  // const headers = {
-  //   "content-type": assetContentType,
-  //   "content-length": contentLength(assetPath),
-  // };
-
-  // // Upload a release asset
-  // // API Documentation: https://developer.github.com/v3/repos/releases/#upload-a-release-asset
-  // // Octokit Documentation: https://octokit.github.io/rest.js/#octokit-routes-repos-upload-release-asset
-  // const uploadAssetResponse = await github.repos.uploadReleaseAsset({
-  //   url: uploadUrl,
-  //   headers,
-  //   name: assetName,
-  //   file: fs.readFileSync(assetPath),
-  // });
 
   console.log(`${JSON.stringify(fs.readdirSync("./"), undefined, 2)}`);
-  console.log(`${JSON.stringify(github.context, undefined, 2)}`);
-  console.log(`${JSON.stringify(github, undefined, 2)}`);
 
-  // await octokit.rest.repos.uploadReleaseAsset({
-  //   ...github.context.repo,
-  //   release_id: release.id,
-  //   name: `${release.name}-asset`,
-  //   data: null, //fs.readFileSync(assetPath),
-  // });
+  await octokit.rest.repos.uploadReleaseAsset({
+    ...github.context.repo,
+    release_id: release.id,
+    name: `${release.name}-asset`,
+    data: fs.readFileSync(assetPath),
+  });
 }
 
 })();
